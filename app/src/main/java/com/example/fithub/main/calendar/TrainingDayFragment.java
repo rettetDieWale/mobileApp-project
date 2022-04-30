92f74bb5b407c0f757e2f0fd1e7e30882a3cac1a
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-04-30 12:38:11 +0200

    added fragment for training days according to #25

---
+++ app/src/main/java/com/example/fithub/main/calendar/TrainingDayFragment.java
@@ -0,0 +1,34 @@
+package com.example.fithub.main.calendar;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fithub.databinding.FragmentTrainingDayBinding;
+
+public class TrainingDayFragment extends Fragment {
+
+    private FragmentTrainingDayBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentTrainingDayBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
