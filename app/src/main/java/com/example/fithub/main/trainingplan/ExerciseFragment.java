7b3a86a4c6290713fb737faa6b7bd642ebae746f
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-05-01 13:47:08 +0200

    added spinner for #34

--- app/src/main/java/com/example/fithub/main/trainingplan/ExerciseFragment.java
+++ app/src/main/java/com/example/fithub/main/trainingplan/ExerciseFragment.java
@@ -9,6 +9,8 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.fithub.R;
 import com.example.fithub.databinding.FragmentExerciseBinding;
@@ -27,12 +29,25 @@ public class ExerciseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         binding = FragmentExerciseBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.template_array,
+                android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
         super.onViewCreated(view, savedInstanceState);
     }
 
