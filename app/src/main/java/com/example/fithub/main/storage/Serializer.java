e3e069a052de5037ae32526ad7a976a732049c5a
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-05-07 17:23:20 +0200

    added comments for de- and serializing

--- app/src/main/java/com/example/fithub/main/storage/Serializer.java
+++ app/src/main/java/com/example/fithub/main/storage/Serializer.java
@@ -7,8 +7,15 @@ import com.google.gson.Gson;
 /** Class for converting objects into json format. */
 public class Serializer {
 
+  /** gson converter object */
   Gson gson;
 
+  /**
+   * Serialize objects into json and stores them into a file.
+   *
+   * @param context of the current activity
+   * @param object to be serialized
+   */
   public void serialize(Context context, Object object) {
 
     this.gson = new Gson();
@@ -18,6 +25,13 @@ public class Serializer {
     storage.storeData(context, "Demo.txt", json);
   }
 
+  /**
+   * Deserialize objects back from json into type.
+   *
+   * @param context of current activity
+   * @param type of class that json should be deserialized into.
+   * @return
+   */
   public Object deserialize(Context context, Class<?> type) {
 
     Storage storage = new Storage();
