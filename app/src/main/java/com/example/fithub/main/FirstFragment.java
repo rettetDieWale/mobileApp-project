222541fbb33b73cb0f358de5951da28be266aa61
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-05-07 11:27:48 +0200

    refactoring component and views init into onCreate
    needed so components can be accessed later

--- app/src/main/java/com/example/fithub/main/FirstFragment.java
+++ app/src/main/java/com/example/fithub/main/FirstFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -15,7 +16,6 @@ import androidx.fragment.app.Fragment;
 import com.example.fithub.R;
 import com.example.fithub.databinding.FragmentFirstBinding;
 import com.example.fithub.main.calendar.CalendarActivity;
-import com.example.fithub.main.storage.Storage;
 
 public class FirstFragment extends Fragment {
 
@@ -25,43 +25,38 @@ public class FirstFragment extends Fragment {
   @Override
   public View onCreateView(
       LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    binding = FragmentFirstBinding.inflate(inflater, container, false);
 
+    // binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+    /**
+     * View view = inflater.inflate(R.layout.fragment_first, container, false); TextView textView =
+     * (TextView) view.findViewById(R.id.text_view_storage);
+     */
     View view = inflater.inflate(R.layout.fragment_first, container, false);
-    TextView textView = (TextView) view.findViewById(R.id.text_view_storage);
+    Button calendarButton = (Button) view.findViewById(R.id.button_home);
 
-    return binding.getRoot();
-  }
+    TextView textview = view.findViewById(R.id.stored_data);
+    textview.setText("HALLO WELT");
 
-  public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
+    // setup progress bar final ProgressBar progressBar = (ProgressBar) final ProgressBar
+    ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+    progressBar.setMax(100);
+    progressBar.setProgress(50);
 
-    binding.buttonHome.setOnClickListener(
+    calendarButton.setOnClickListener(
         new View.OnClickListener() {
           @Override
           public void onClick(View v) {
-            /**
-             * NavHostFragment.findNavController(FirstFragment.this)
-             * .navigate(R.id.action_FirstFragment_to_calenderFragment);
-             */
             Intent intent = new Intent(getActivity(), CalendarActivity.class);
             startActivity(intent);
           }
         });
 
-    binding.buttonSave.setOnClickListener(
-        new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-            Storage storage = new Storage();
-            storage.storeData(getActivity());
-          }
-        });
+    return view;
+  }
 
-    // setup progress bar
-    final ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
-    progressBar.setMax(100);
-    progressBar.setProgress(50);
+  public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
   }
 
   @Override
