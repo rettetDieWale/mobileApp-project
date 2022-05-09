79eeb9b24a5f5ebc9a27017dfcf5634d364dba5f
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-05-09 16:58:56 +0200

    refactoring for more code quality

--- app/src/main/java/com/example/fithub/main/PieChartFragment.java
+++ app/src/main/java/com/example/fithub/main/PieChartFragment.java
@@ -11,7 +11,6 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.fithub.R;
-import com.example.fithub.databinding.FragmentPiechartBinding;
 import com.example.fithub.main.prototypes.MuscleGroupChart;
 import com.example.fithub.main.storage.Savefile;
 import com.example.fithub.main.storage.Serializer;
@@ -22,33 +21,28 @@ import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
 public class PieChartFragment extends Fragment {
 
-  private FragmentPiechartBinding binding;
   private PieChart chart;
   private MuscleGroupChart muscleGroupChart;
 
   @Override
   public View onCreateView(
       LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    final View view = inflater.inflate(R.layout.fragment_piechart, container, false);
-
-    return view;
+    return inflater.inflate(R.layout.fragment_piechart, container, false);
   }
 
   public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
     initChart();
+    this.chart = (PieChart) view.findViewById(R.id.chart1);
 
-    chart = (PieChart) view.findViewById(R.id.chart1);
-
-    List<PieEntry> pieEntries = new ArrayList<>();
-    for (Map.Entry<String, String> entry : muscleGroupChart.getAllData().entrySet()) {
+    final List<PieEntry> pieEntries = new ArrayList<>();
+    for (Map.Entry<String, String> entry : this.muscleGroupChart.getAllData().entrySet()) {
       pieEntries.add(new PieEntry(Float.parseFloat(entry.getValue()), entry.getKey()));
     }
 
@@ -58,9 +52,9 @@ public class PieChartFragment extends Fragment {
   }
 
   /** Fills the pie chart with initial data. Useful when files are corrupted or not existing. */
-  public void resetChart() {
+  private void resetChart() {
     // raw test data for testing purposes only
-    ArrayMap<String, String> MuscleData = new ArrayMap<>();
+    final ArrayMap<String, String> MuscleData = new ArrayMap<>();
     MuscleData.put("Schultern", "18.5f");
     MuscleData.put("Brust", "26.7f");
     MuscleData.put("Rücken", "10.1f");
@@ -68,8 +62,8 @@ public class PieChartFragment extends Fragment {
     MuscleData.put("Beine", "30.8f");
     MuscleData.put("Bauch", "11.4f");
 
-    muscleGroupChart = new MuscleGroupChart();
-    muscleGroupChart.addDataAll(MuscleData);
+    this.muscleGroupChart = new MuscleGroupChart();
+    this.muscleGroupChart.addDataAll(MuscleData);
   }
 
   /**
@@ -77,11 +71,11 @@ public class PieChartFragment extends Fragment {
    *
    * @param pieEntries that are rendered into the chart.
    */
-  public void renderChart(List<PieEntry> pieEntries) {
-    PieDataSet set = new PieDataSet(pieEntries, "Muskelgruppen trainiert");
-    chart.setEntryLabelColor(Color.BLACK);
+  private void renderChart(final List<PieEntry> pieEntries) {
+    final PieDataSet set = new PieDataSet(pieEntries, "Muskelgruppen trainiert");
+    this.chart.setEntryLabelColor(Color.BLACK);
 
-    int[] Colors = {
+    final int[] Colors = {
       Color.rgb(79, 118, 247),
       Color.rgb(146, 79, 247),
       Color.rgb(124, 247, 79),
@@ -92,8 +86,8 @@ public class PieChartFragment extends Fragment {
 
     set.setColors(Colors);
     final PieData data = new PieData(set);
-    chart.setData(data);
-    chart.invalidate(); // refresh data - no idea why its called invalidate in lib
+    this.chart.setData(data);
+    this.chart.invalidate(); // refresh data - no idea why its called invalidate in lib
   }
 
   /**
@@ -101,17 +95,14 @@ public class PieChartFragment extends Fragment {
    *
    * @param pieEntries to be sorted
    */
-  public void sortChart(List<PieEntry> pieEntries) {
+  private void sortChart(final List<PieEntry> pieEntries) {
     Collections.sort(
         pieEntries,
-        new Comparator<Object>() {
-          @Override
-          public int compare(Object a1, Object a2) {
-            PieEntry pe1 = (PieEntry) a1;
-            PieEntry pe2 = (PieEntry) a2;
-            // sorting so biggest value starts on right side of the donut chart
-            return Float.compare(pe2.getValue(), pe1.getValue());
-          }
+        (Object a1, Object a2) -> {
+          final PieEntry pe1 = (PieEntry) a1;
+          final PieEntry pe2 = (PieEntry) a2;
+          // sorting so biggest value starts on right side of the donut chart
+          return Float.compare(pe2.getValue(), pe1.getValue());
         });
   }
 
@@ -119,8 +110,8 @@ public class PieChartFragment extends Fragment {
    * Initializes the chart data based on savefile or if file is not existing/corrupted with example
    * data.
    */
-  public void initChart() {
-    Serializer serializer = new Serializer();
+  private void initChart() {
+    final Serializer serializer = new Serializer();
     this.muscleGroupChart =
         (MuscleGroupChart)
             serializer.deserialize(
@@ -134,6 +125,5 @@ public class PieChartFragment extends Fragment {
   @Override
   public void onDestroyView() {
     super.onDestroyView();
-    binding = null;
   }
 }
