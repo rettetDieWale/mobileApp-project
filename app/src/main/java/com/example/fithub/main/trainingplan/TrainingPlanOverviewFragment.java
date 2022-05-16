1e3c83b354ac1aed76e9dc86f62c4c9e885610b4
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-05-16 20:40:29 +0200

    added list to training plan overview

--- app/src/main/java/com/example/fithub/main/trainingplan/TrainingPlanOverviewFragment.java
+++ app/src/main/java/com/example/fithub/main/trainingplan/TrainingPlanOverviewFragment.java
@@ -9,11 +9,19 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fithub.R;
 import com.example.fithub.main.calendar.CalendarActivity;
+import com.example.fithub.main.components.ListAdapter;
 
 public class TrainingPlanOverviewFragment extends Fragment {
+
+  private final String[] names = {
+    "hello", "bye", "main", "joe", "give", "true", "false", "know", "list", "hello", "bye"
+  };
 
   @Override
   public View onCreateView(
@@ -29,6 +37,21 @@ public class TrainingPlanOverviewFragment extends Fragment {
             startActivity(intent);
           }
         });
+
+    // ONLY FOR TEST PURPOSE WILL BE DELETED LATER
+    final Button tpButton = view.findViewById(R.id.button_tp_for_test);
+    tpButton.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            NavHostFragment.findNavController(TrainingPlanOverviewFragment.this)
+                .navigate(R.id.action_training_plan_overview_to_training_plan);
+          }
+        });
+
+    final RecyclerView recyclerView = view.findViewById(R.id.listLayout);
+    recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    recyclerView.setAdapter(new ListAdapter(names));
 
     return view;
   }
