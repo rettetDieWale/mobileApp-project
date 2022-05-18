0273dfc8338d255afc585ee1fe2dafb46910c794
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-05-18 16:57:37 +0200

    Exercise class for storage and serialization created

---
+++ app/src/main/java/com/example/fithub/main/prototypes/Exercise.java
@@ -0,0 +1,40 @@
+package com.example.fithub.main.prototypes;
+
+public class Exercise {
+  private String name;
+  private String instruction;
+  private String imageUrl;
+  private String videoUrl;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getInstruction() {
+    return instruction;
+  }
+
+  public void setInstruction(String instruction) {
+    this.instruction = instruction;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+  public String getVideoUrl() {
+    return videoUrl;
+  }
+
+  public void setVideoUrl(String videoUrl) {
+    this.videoUrl = videoUrl;
+  }
+}
