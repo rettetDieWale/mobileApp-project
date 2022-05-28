287cbb939d74d73ac05384e9f4a39f494e45e8d5
nesti024 <56504331+nesti024@users.noreply.github.com>
2022-05-28 12:56:56 +0200

    TrainingsPlanFragment wird geshared

--- app/src/main/java/com/example/fithub/main/calendar/CalendarActivity.java
+++ app/src/main/java/com/example/fithub/main/calendar/CalendarActivity.java
@@ -1,14 +1,7 @@
 package com.example.fithub.main.calendar;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CalendarView;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
--- app/src/main/java/com/example/fithub/main/prototypes/data/AppDatabase.java
+++ app/src/main/java/com/example/fithub/main/prototypes/data/AppDatabase.java
@@ -2,19 +2,25 @@ package com.example.fithub.main.prototypes.data;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
+import com.example.fithub.main.Converters;
 import com.example.fithub.main.prototypes.data.daos.ExerciseDataDao;
 import com.example.fithub.main.prototypes.data.daos.PlanEntryDao;
+import com.example.fithub.main.prototypes.data.daos.TrainingDayDao;
 import com.example.fithub.main.prototypes.data.daos.TrainingPlanDao;
 
 /** Singleton database object for application. */
 @Database(
-    entities = {TrainingPlan.class, PlanEntry.class, ExerciseData.class},
+    entities = {TrainingPlan.class, PlanEntry.class, ExerciseData.class, TrainingDay.class},
     version = 1)
+@TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
   public abstract TrainingPlanDao trainingPlanDao();
 
   public abstract ExerciseDataDao exerciseDataDao();
 
   public abstract PlanEntryDao planEntryDao();
+
+  public abstract TrainingDayDao trainingDayDao();
 }
