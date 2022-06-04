009cb34a3b2e8cec303c099715c98e982cb50ae1
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-06-04 09:59:04 +0200

    added button to store training day in database

--- app/src/main/java/com/example/fithub/main/calendar/TrainingDayFragment.java
+++ app/src/main/java/com/example/fithub/main/calendar/TrainingDayFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -35,8 +36,6 @@ public class TrainingDayFragment extends Fragment {
     // Inflate the layout for this fragment
     this.view = inflater.inflate(R.layout.fragment_training_day, container, false);
 
-    // ToDo:!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    DatabaseManager.appDatabase.trainingDayDao().insert(new TrainingDay(0, new Date(), 1, 1));
     // -------------------------------------------------------------
     final FragmentManager fragmentManager = getChildFragmentManager();
     final List<Fragment> fragmentList = fragmentManager.getFragments();
@@ -45,6 +44,17 @@ public class TrainingDayFragment extends Fragment {
     b.putInt("trainingPlanId", 1);
     b.putInt("actionId", 1);
     fragmentList.get(0).setArguments(b);
+
+    final Button saveButton = this.view.findViewById(R.id.save_training_day);
+    saveButton.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            DatabaseManager.appDatabase
+                .trainingDayDao()
+                .insert(new TrainingDay(0, new Date(), 1, 1));
+          }
+        });
 
     return view;
   }
