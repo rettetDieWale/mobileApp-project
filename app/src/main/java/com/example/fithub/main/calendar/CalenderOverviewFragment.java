f0267fb5aee3da95269dfe710549cec68b57b99e
sdresselmann <stephan.dresselmann@fh-bielefeld.de>
2022-06-10 22:42:12 +0200

    changing buttons in training day layout for light and night mode

--- app/src/main/java/com/example/fithub/main/calendar/CalenderOverviewFragment.java
+++ app/src/main/java/com/example/fithub/main/calendar/CalenderOverviewFragment.java
@@ -26,65 +26,64 @@ import java.util.List;
 
 public class CalenderOverviewFragment extends Fragment {
 
-    private CalendarView simpleCalendarView;
+  private CalendarView simpleCalendarView;
 
-    @Nullable
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+  @Nullable
+  @Override
+  public View onCreateView(
+      LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        final View view = inflater.inflate(R.layout.fragment_calender_overview, container, false);
+    final View view = inflater.inflate(R.layout.fragment_calender_overview, container, false);
 
-        CompactCalendarView compactCalendarView = view.findViewById(R.id.compactcalendar_view);
-        compactCalendarView.setUseThreeLetterAbbreviation(true);
-        TextView currentMonthTextView = view.findViewById(R.id.month);
-        Calendar cal=Calendar.getInstance();
-        SimpleDateFormat month_date = new SimpleDateFormat("MMMM");
-        String month_name = month_date.format(cal.getTime());
+    CompactCalendarView compactCalendarView = view.findViewById(R.id.compactcalendar_view);
+    compactCalendarView.setUseThreeLetterAbbreviation(true);
+    TextView currentMonthTextView = view.findViewById(R.id.month);
+    Calendar cal = Calendar.getInstance();
+    SimpleDateFormat month_date = new SimpleDateFormat("MMMM");
+    String month_name = month_date.format(cal.getTime());
 
-        currentMonthTextView.setText(month_name);
+    currentMonthTextView.setText(month_name);
 
+    // set events
+    List<TrainingDay> trainingDays = DatabaseManager.appDatabase.trainingDayDao().getAll();
 
-        // set events
-        List<TrainingDay> trainingDays = DatabaseManager.appDatabase.trainingDayDao().getAll();
+    for (int i = 0; i < trainingDays.size(); i++) {
+      Date trainingDayDate = trainingDays.get(i).getDate();
 
-        for (int i = 0; i < trainingDays.size(); i++) {
-            Date trainingDayDate = trainingDays.get(i).getDate();
-
-            Event event = new Event(Color.BLUE, trainingDayDate.getTime(), "training_day");
-            compactCalendarView.addEvent(event, true);
-        }
-
-        compactCalendarView.setListener(
-                new CompactCalendarView.CompactCalendarViewListener() {
-                    @Override
-                    public void onDayClick(Date dateClicked) {
-                        Bundle args = new Bundle();
-                        args.putSerializable("date", dateClicked);
-                        NavHostFragment.findNavController(CalenderOverviewFragment.this)
-                                .navigate(R.id.action_calenderOverviewFragment_to_trainingDayFragment, args);
-                    }
-
-                    @Override
-                    public void onMonthScroll(Date firstDayOfNewMonth) {
-                        TextView currentMonthTextView = view.findViewById(R.id.month);
-                        Calendar cal=Calendar.getInstance();
-                        cal.setTime(compactCalendarView.getFirstDayOfCurrentMonth());
-                        SimpleDateFormat month_date = new SimpleDateFormat("MMMM");
-                        String month_name = month_date.format(cal.getTime());
-                        currentMonthTextView.setText(month_name);
-                    }
-                });
-
-        return view;
+      Event event = new Event(Color.WHITE, trainingDayDate.getTime(), "training_day");
+      compactCalendarView.addEvent(event, true);
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
+    compactCalendarView.setListener(
+        new CompactCalendarView.CompactCalendarViewListener() {
+          @Override
+          public void onDayClick(Date dateClicked) {
+            Bundle args = new Bundle();
+            args.putSerializable("date", dateClicked);
+            NavHostFragment.findNavController(CalenderOverviewFragment.this)
+                .navigate(R.id.action_calenderOverviewFragment_to_trainingDayFragment, args);
+          }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-    }
+          @Override
+          public void onMonthScroll(Date firstDayOfNewMonth) {
+            TextView currentMonthTextView = view.findViewById(R.id.month);
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(compactCalendarView.getFirstDayOfCurrentMonth());
+            SimpleDateFormat month_date = new SimpleDateFormat("MMMM");
+            String month_name = month_date.format(cal.getTime());
+            currentMonthTextView.setText(month_name);
+          }
+        });
+
+    return view;
+  }
+
+  public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+  }
 }
